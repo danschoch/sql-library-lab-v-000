deafaf4eb0114f45c5c158907f8ccc7db6d63105
Daniel Schoch <d.schoch90@gmail.com>
2018-10-02 08:45:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON series.id = characters.series_id
   GROUP BY characters.species HAVING characters.species = 'human'
-  ORDER BY characters.species DESC"
+  ORDER BY characters.species ASC"
 end
 
 def select_character_names_and_number_of_books_they_are_in
