fd67a2dc626471a2cffcb98258b72e97df9e4aea
Daniel Schoch <d.schoch90@gmail.com>
2018-10-02 08:39:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,8 +24,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  JOIN series WHERE authors.id = series.author_id
-  JOIN subgenres WHERE subgenres.id = series.id"
+  JOIN series ON authors.id = series.author_id
+  JOIN subgenres ON subgenres.id = series.id"
 end
 
 def select_series_title_with_most_human_characters
